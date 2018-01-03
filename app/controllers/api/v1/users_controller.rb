2758fcd5dac93179021fb3f91649e48584bdf5fa
Lorena Bento <lorenadgb@gmail.com>
2018-01-03 00:03:37 -0200

    adding create action to the users controller with tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,7 +8,22 @@ class Api::V1::UsersController < ApplicationController
     rescue
       head 404
     end
+  end
 
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :user do
     name 'Lorena'
-    email 'lorenadgb@gmail.com'
+    email Faker::Internet.email
     password '123456'
     password_confirmation '123456'
   end
